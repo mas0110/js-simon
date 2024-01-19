5f16d428753e427cb5816ef769f064ec06fe3163
mas0110 <masoudmohammed13@gmail.com>
2024-01-19 10:16:07 +0100

    correzione del conto

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 // sfruttiamo le timing functions per fare il conto alla rovescia per la correzione di domani! Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina!
 // 
 
-const termine = new Date("Jan 19, 2024 09:30:00").getTime();
+const termine = new Date("Jan 20, 2024 09:30:00").getTime();
 
 const countdown = setInterval(function(){
 
     let today = new Date().getTime();
-    let distance = countdown - today;
+    let distance = termine - today;
 
     let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
     let minute = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
@@ -14,14 +14,14 @@ const countdown = setInterval(function(){
 
      
     let insieme = hours + "h "+ minute + "m " + seconds + "s ";
-    document.getElementById("conto").innerHTML = insieme
+    document.getElementById("conto").innerHTML = insieme;
     console.log()
     
       
     
-    if (insieme > 0) {
+    if (distance < 0) {
       clearInterval(countdown);
       document.getElementById("conto").innerHTML = "countdown scaduto";
-  }
+    }
     
 },1000)
